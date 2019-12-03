8dba665bf5a8cb6db4aaa7a308a3b31f7811bb34
Therese <56429898+2017070162@users.noreply.github.com>
2019-12-03 22:29:44 +0800

    11

---
+++ app.js
@@ -0,0 +1,79 @@
+// jshint esversion:8
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+app.use(express.static("public"));
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.set('view engine', 'ejs');
+
+app.get("/about", (req, res)=>{
+  res.render("about");
+});
+app.get("/blog", (req, res)=>{
+  res.render("blog");
+});
+app.get("/contactdonate", (req, res) => {
+  res.render("contactdonate");
+});
+app.get("/donate", (req, res) => {
+  res.render("donate");
+});
+app.get("/event", (req, res) => {
+  res.render("event");
+});
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use("/", require("./routes/index.js"));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
